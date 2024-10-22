49488a28556f68a72e65be75929e67003545d34f
mocsow <mcsow@alumni.chicagobooth.edu>
2024-10-22 02:46:11 +0000

    231021: 2246

--- dice.rb
+++ dice.rb
@@ -2,7 +2,8 @@ require "sinatra"
 require "sinatra/reloader"
 require "better_errors"
 require "binding_of_caller"
-
+  
+# Need this configuration for better_errors
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
@@ -11,69 +12,50 @@ get("/") do
   erb(:elephant)
 end
 
-#get ("/") do
-  "<h1>Dice Roll</h1>
-
-  <ul>
-    <li><a href = https://studious-guide-97jj9w9vvwwj2pr4w-4567.app.github.dev/dice/2/6>Roll two 6-sided dice</a></li>
-
-    <li><a href = https://studious-guide-97jj9w9vvwwj2pr4w-4567.app.github.dev/dice/2/10>Roll two 10-sided dice</a></li>
-
-    <li><a href = https://studious-guide-97jj9w9vvwwj2pr4w-4567.app.github.dev/dice/1/20>Roll one 20-sided die</a></li>
-
-    <li><a href = https://studious-guide-97jj9w9vvwwj2pr4w-4567.app.github.dev/dice/5/4>Roll five 4-sided die</a></li>
-  </ul>" 
-#end
-
-get ("/zebra") do
+get("/zebra") do
   "We must add a route for each path we want to support"
 end
 
 get("/giraffe") do
-  "We will not have to restart the server anymore."
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
-get('/dice/2/6') do
+get("/dice/2/6") do
   first_die = rand(1..6)
-  second_die = rand(1..6)
+	second_die = rand(1..6)
   sum = first_die + second_die
-
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-
-  erb(:two_six)
+	
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+	erb(:two_six)
 end
 
-get('/dice/2/10') do
+get("/dice/2/10") do
   first_die = rand(1..10)
-  second_die = rand(1..10)
+	second_die = rand(1..10)
   sum = first_die + second_die
-
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-
-   "<h1>2d10</h1>
-    <p>#{outcome}</p>"
+	
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  erb(:two_ten)
 end
 
-get('/dice/1/20') do
-  first_die = rand(1..20)
-  sum = first_die
-
-  outcome = "You rolled a #{first_die} for a total of #{sum}"
-
-  "<h1>1d20</h1>
-    <p>#{outcome}</p>"
+get("/dice/1/20") do
+  die = rand(1..20)
+	
+	@outcome = "You rolled a #{die}."
+	
+	erb(:one_twenty)
 end
 
 get("/dice/5/4") do
   first_die = rand(1..4)
-  second_die = rand(1..4)
+	second_die = rand(1..4)
   third_die = rand(1..4)
   fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-  
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}" 
-  
-  "<h1>5d4</h1>
-    <p>#{outcome}</p>"
+  sum = first_die + second_die + third_die + fourth_die
+	
+	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	
+  erb(:five_four)
 end
